16f2ad650f2e25c1494380fce255f3aebe7b5e51
KokiYoshikawa <yskw.kk.git@gmail.com>
2024-01-29 15:03:31 +0900

    mysql修正

--- sql/test.sql
+++ sql/test.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS user
   `first_name_kana` VARCHAR(50),
   `last_name_kana` VARCHAR(50),
   `mail_address` VARCHAR(100),
+  `pass_word` VARCHAR(50),
   PRIMARY KEY (`user_id`)
 );
 
-INSERT INTO user (first_name, last_name, first_name_kana, last_name_kana, mail_address) VALUES
-(
-  "姓", "名", "せい", "めい", "sample@xxx.com"
-);
+INSERT INTO user (first_name, last_name, first_name_kana, last_name_kana, mail_address, pass_word) VALUES
+("姓", "名", "せい", "めい", "sample@xxx.com", "11111111"),
+("姓", "名", "せい", "めい", "sample2@xxx.com", "22222222");
