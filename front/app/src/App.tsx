103ed5d6ec6ff846997853178ed8e66ec405ebb8
KokiYoshikawa <yskw.kk.git@gmail.com>
2024-02-10 21:08:46 +0900

    リロードしてもログアウトしないように修正

--- front/app/src/App.tsx
+++ front/app/src/App.tsx
@@ -20,29 +20,35 @@ import NotFound from './components/NotFound';
 import { LoginCheck, AuthCheck } from './components/Auth/Check';
 import store from './redux/Store';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store)
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user/:id" element={<LoginCheck><UserDetail/></LoginCheck>} />
-            <Route path="/user/list" element={<LoginCheck><UserList/></LoginCheck>} />
-            <Route path="/user/create" element={<AuthCheck><UserCreate/></AuthCheck>} />
-            <Route path="/user/delete/:id" element={<AuthCheck><UserDelete/></AuthCheck>} />
-            <Route path="/user/complete" element={<AuthCheck><UserComplete/></AuthCheck>} />
-            <Route path="/admin/login" element={<AdminLogin/>} />
-            <Route path="/admin/:id" element={<LoginCheck><AdminDetail/></LoginCheck>} />
-            <Route path="/admin/list" element={<LoginCheck><AdminList/></LoginCheck>} />
-            <Route path="/admin/create" element={<AuthCheck><AdminCreate/></AuthCheck>} />
-            <Route path="/admin/delete/:id" element={<AuthCheck><AdminDelete/></AuthCheck>} />
-            <Route path="/admin/complete" element={<AuthCheck><AdminComplete/></AuthCheck>} />
-            <Route path="/admin/noauth" element={<NoAuth/>} />
-            <Route path="/*" element={<NotFound/>} />
-          </Routes>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/user/:id" element={<LoginCheck><UserDetail/></LoginCheck>} />
+              <Route path="/user/list" element={<LoginCheck><UserList/></LoginCheck>} />
+              <Route path="/user/create" element={<AuthCheck><UserCreate/></AuthCheck>} />
+              <Route path="/user/delete/:id" element={<AuthCheck><UserDelete/></AuthCheck>} />
+              <Route path="/user/complete" element={<AuthCheck><UserComplete/></AuthCheck>} />
+              <Route path="/admin/login" element={<AdminLogin/>} />
+              <Route path="/admin/:id" element={<LoginCheck><AdminDetail/></LoginCheck>} />
+              <Route path="/admin/list" element={<LoginCheck><AdminList/></LoginCheck>} />
+              <Route path="/admin/create" element={<AuthCheck><AdminCreate/></AuthCheck>} />
+              <Route path="/admin/delete/:id" element={<AuthCheck><AdminDelete/></AuthCheck>} />
+              <Route path="/admin/complete" element={<AuthCheck><AdminComplete/></AuthCheck>} />
+              <Route path="/admin/noauth" element={<NoAuth/>} />
+              <Route path="/*" element={<NotFound/>} />
+            </Routes>
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
--- front/app/src/redux/Store.tsx
+++ front/app/src/redux/Store.tsx
@@ -1,9 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { rootReducer } from './Reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import thunk from 'redux-thunk';
 
-const store = configureStore({reducer: rootReducer});
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // middleware: [thunk]
+});
 
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//   ReturnType,
+//   RootState,
+//   unknown,
+//   Action<string>
+// >;
